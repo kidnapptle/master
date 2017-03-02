055f97c09b1baeceae8c41370bea0fc7b9bdf232
kidnapptle <thanakorn_tle@hotmail.com>
2017-03-02 15:31:57 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -29,7 +29,7 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text' && $event['message']['text'] = 'Gutle') {
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text' && $event['message']['text'] == 'Gutle') {
 			// Get text sent
 			$text = $event['message']['text'];
 			// Get replyToken
