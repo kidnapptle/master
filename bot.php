e0136b6621e6a97b345c63cac6653149830055a1
kidnapptle <thanakorn_tle@hotmail.com>
2017-03-02 23:53:26 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -27,40 +27,8 @@ foreach (getallheaders() as $name => $value) {
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
-
-		// Make a Fetch user profile
-		$uid = $event['source']['userId'];
-
-		$url = 'https://api.line.me/v2/bot/profile/'.$uid;
-		$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-		$ch = curl_init($url);
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, 0);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-		$result = curl_exec($ch);
-		curl_close($ch);
-
-		$profileinfo = json_decode($result, true);
-
-
-		// Retrieve image picture
-		$urlprofile = $profileinfo['pictureUrl'].'/large';
-
-		$chpic = curl_init($urlprofile);
-		$saveuri = "/usr/share/nginx/html/profile/".$uid.".png";
-		$temppic = fopen($saveuri,"wb");
-		curl_setopt($chpic, CURLOPT_FILE, $temppic);
-		curl_setopt($chpic, CURLOPT_HEADER, 0);
-		curl_setopt($chpic, CURLOPT_FOLLOWLOCATION, true);
-		$resultpic = curl_exec($chpic);
-		curl_close($chpic);
-		fclose($saveuri);
-
-		// Reply only when message sent is in 'text' format and match word Demo
-		if ($event['type'] == 'message' && ($event['message']['type'] == 'text' && $event['message']['text'] == 'Demo') ) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
 			// Get replyToken
@@ -69,13 +37,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$message1 = [
 				'type' => 'text',
-				'text' => 'hello '.$profileinfo['displayName'].' '.$profileinfo['statusMessage']
-			];
-
-			$message4 = [
-				'type' => 'image',
-				'originalContentUrl' => 'https://iservices.me/profile/'.$uid.'.png',
-				'previewImageUrl' => 'https://iservices.me/profile/'.$uid.'.png'
+				'text' => $text
 			];
 
 			$message2 = [
@@ -143,13 +105,11 @@ if (!is_null($events['events'])) {
 				)
 			];
 
-
-
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => array($message1,$message4,$message2,$message3),
+				'messages' => array($message1,$message2,$message3),
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
