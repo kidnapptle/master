ae869988cfe537c814284257b2f7da26f54ba03d
kidnapptle <thanakorn_tle@hotmail.com>
2017-03-02 22:14:35 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -82,13 +82,6 @@ if (!is_null($events['events'])) {
 										'type' => 'message',
 										'label' => 'Say Hello',
 										'text' => 'Hello'
-									array(
-										"type": "location",
-    "title": "my location",
-    "address": "〒150-0002 東京都渋谷区渋谷２丁目２１−１",
-    "latitude": 35.65910807942215,
-    "longitude": 139.70372892916203
-									)
 									)
 								)
 							),
